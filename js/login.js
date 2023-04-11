10972f4d0f8a257e049c5ad207270295eb8a916a
Isaiah Clark <Isaiah@Isaiahs-MacBook-Air.local>
2023-04-11 12:39:41 -0500

    Added js file for login page

---
+++ js/login.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("login-btn");
+const btnClicked = () => {
+    let user = document.getElementById("username-input").innerHTML;
+    let pass = document.getElementById("password-input").innerHTML;
+    
+};
